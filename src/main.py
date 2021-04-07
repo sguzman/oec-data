bc4af3a0b560bdd1e6f6e24c2c838e7bd336aee9
Salvador Guzman <guzmansalv@gmail.com>
2021-04-07 11:40:02 -0700

    working now

--- src/main.py
+++ src/main.py
@@ -1,16 +1,40 @@
+import atexit
 import requests
+from typing import Dict
+from typing import List
+
+import oec
+import oec.misc
 
 
 def init_log() -> None:
     print('hi')
 
 
+def init_atexit() -> None:
+    def exit_func() -> None:
+        print('bye')
+
+    atexit.register(exit_func)
+
+
 def init() -> None:
     init_log()
 
 
+depth: List[str] = ['hs4', 'hs6']
+rev: List[str] = [
+    'hs92',
+    'hs96',
+    'hs02',
+    'hs07'
+]
+
+
 def main() -> None:
     init()
+    print(oec.misc.countries())
+    print(oec.misc.prods('hs6', 'hs07'))    
 
 
 if __name__ == '__main__':
---
+++ src/oec/misc.py
@@ -0,0 +1,56 @@
+from typing import Dict
+from typing import List
+
+import json
+import requests
+
+url_country: str = 'https://oec.world/olap-proxy/data.jsonrecords'
+param_country: Dict[str, str] = {
+    'cube': 'complexity_eci_a_hs96_hs6',
+    'Year': 2019,
+    'drilldowns': 'Country',
+    'measures': 'ECI',
+    'parents': 'true',
+    'sparse': 'false'
+}
+
+
+def countries() -> List[str]:
+    resp: str = requests.get(url_country, params=param_country).text
+    js_obj: Dict = json.loads(resp)
+
+    cs: List[str] = []
+    for c in js_obj['data']:
+        obj: str = c['Country ID']
+        cs.append(obj)
+
+    return cs
+
+
+url_prod: str = 'https://oec.world/olap-proxy/data.jsonrecords'
+
+
+def prod_params(depth: str, rev: str) -> Dict[str, str]:
+    param_prod: Dict[str, str] = {
+        'cube': f'complexity_pci_a_{rev}_{depth}',
+        'Year': 2019,
+        'drilldowns': depth.upper(),
+        'measures': 'PCI',
+        'parents': 'true',
+        'sparse': 'false',
+    }
+
+    return param_prod
+
+
+def prods(depth: str, rev: str) -> List[str]:
+    params: Dict[str, str] = prod_params(depth, rev)
+    resp: str = requests.get(url_prod, params=params).text
+    js_obj: Dict = json.loads(resp)
+
+    pd: List[str] = []
+    for c in js_obj['data']:
+        obj: str = c[f'{depth.upper()} ID']
+        pd.append(obj)
+
+    return pd
