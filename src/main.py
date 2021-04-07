2c7802872c82b7e01a533022fffa85614dbc9254
Salvador Guzman <guzmansalv@gmail.com>
2021-04-07 10:50:15 -0700

    src stub

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def init_log() -> None:
+    print('hi')
+
+
+def init() -> None:
+    init_log()
+
+
+def main() -> None:
+    init()
+
+
+if __name__ == '__main__':
+    main()
